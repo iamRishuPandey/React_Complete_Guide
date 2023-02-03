c0ba306592b115b5b68cf6958bb1c19539f40d5f
iamRishuPandey <111633192+iamRishuPandey@users.noreply.github.com>
2023-02-03 19:25:24 +0530

    Add files via upload

---
+++ components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div><h2>Expense item!</h2></div>
+            <div><h2>Food Rs 10</h2></div>
+            <div><h2>Petrol Rs 100</h2></div>
+            <div><h2>Movies Rs 200</h2></div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
