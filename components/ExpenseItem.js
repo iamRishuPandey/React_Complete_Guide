88ef545a74f75f2a2f44f005b22c1a460224e0f3
iamRishuPandey <111633192+iamRishuPandey@users.noreply.github.com>
2023-02-05 22:23:43 +0530

    update Expenses.js

--- components/ExpenseItem.js
+++ components/ExpenseItem.js
@@ -1,10 +1,13 @@
-function ExpenseItem() {
+import './Expenseitem.css';
+function ExpenseItem(props) {
     return (
-        <div>
-            <div><h2>Expense item!</h2></div>
-            <div><h2>Food Rs 10</h2></div>
-            <div><h2>Petrol Rs 100</h2></div>
-            <div><h2>Movies Rs 200</h2></div>
+        <div className="expense-item" >
+            <div>{props.date.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <h2>{props.locationOfExpenditure}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
         </div>
     );
 }
